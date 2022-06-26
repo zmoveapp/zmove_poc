23956c7d2a3c34398664d391dc0f679918864353
Jesus Redondo <jesus.redondo@ft.com>
2022-06-26 10:35:45 +0200

    Fix reference in tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:zmove_poc/main.dart';
+import 'package:zmove_poc/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
