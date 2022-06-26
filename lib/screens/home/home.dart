fd567b335c06a88b6f1bb69925a64204259ba6ab
Jesus Redondo <jesus.redondo@ft.com>
2022-06-26 21:47:18 +0200

    Add 1 image to the assets & show at Home
    Use the relative path to the image in the asset folder to add to the pubspec.yaml configuration file.
    Show it at Home using Column & Expanded widgets to show it 3 times.

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -28,18 +28,15 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: const Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Image(
-            image: NetworkImage(
-                'https://image.made-in-china.com/155f0j00eJqiwAVzlRbt/Double-Crazy-Flutter-Sleeve-Ruffle-Hem-Schiffy-Summer-Sexy-Women-Fashion-Short-Sleeves-Dress.jpg')),
-      ),
-    );
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+        ),
+        body: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          Expanded(child: Image.asset('images/logo.png')),
+          Expanded(child: Image.asset('images/logo.png')),
+          Expanded(child: Image.asset('images/logo.png')),
+        ]));
   }
 }
