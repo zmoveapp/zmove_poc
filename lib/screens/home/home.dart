0ae1db7db38b859dbd9736b0e4eec1af2b175a14
Jesus Redondo <jesus.redondo@ft.com>
2022-06-26 21:22:38 +0200

    Simplify home to work with images

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'home_scaffold.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -19,12 +18,7 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends MyHomePageStateBase with MyIncrement {}
-
-class MyHomePageStateBase extends State<MyHomePage> {
-  int _counter = 0;
-  void _incrementCounter() {}
-
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -33,20 +27,19 @@ class MyHomePageStateBase extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return homeScafold(context, widget);
-  }
-}
-
-mixin MyIncrement on MyHomePageStateBase {
-  @override
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: const Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Image(
+            image: NetworkImage(
+                'https://image.made-in-china.com/155f0j00eJqiwAVzlRbt/Double-Crazy-Flutter-Sleeve-Ruffle-Hem-Schiffy-Summer-Sexy-Women-Fashion-Short-Sleeves-Dress.jpg')),
+      ),
+    );
   }
 }
